4ef4c495715eb025ea57063c22e3bef559f97939
SamWing1 <geckoromanwing@hotmail.com>
2023-01-11 13:35:46 -0800

    test

--- routes/ingredient.js
+++ routes/ingredient.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const ingredientCtrl = require('../controllers/ingredient.js');
 
-router.get('/ingredient/new', ingredientCtrl.new);
+router.get('ingredient/new', ingredientCtrl.new);
 router.post('/ingredient', ingredientCtrl.create);
 router.post('/recipe/:id/ingredient', ingredientCtrl.addToRecipe)
 // router.delete('/recipe/:recipeid/ingredient/:ingredientid', ingredientCtrl.delete);
